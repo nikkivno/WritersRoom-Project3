320a650a2ecf32b7d12886884a229b9906d1c12a
nikkivno <nikkivigneault@gmail.com>
2023-10-03 15:42:23 -0300

    started navbar link connections

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import About from './components/about';
+import Login from './components/login';
 
 function App() {
+    let Component
+    switch (window.location.pathname) {
+        case "/":
+            Component = Login
+            break
+                case "/about":
+                    Component = About
+                    break    
+    }
     return (
     <div>
         <Navbar />
