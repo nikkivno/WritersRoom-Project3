82b2ffd233cbc5fd94580293d2f7bc580551e590
nikkivno <nikkivigneault@gmail.com>
2023-10-02 21:20:16 -0300

    some base code started as a tester for react

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import '../styles/navbar.css';
 
+function Navbar() {
+    const currentPage = window.location.pathname;
+    return (
+        <nav>
+            <h1>Writer's Room</h1>
+            <ul>
+                <li>Login/Sign Up</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    );
+}
 
+export default Navbar; 
 
