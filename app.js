c26c4ed1f171f47718792a17dbf8cdbd62b78588
Scoges <scott.ogrins@gmail.com>
2023-10-03 19:10:25 -0300

    full jwt code

--- app.js
+++ app.js
@@ -1,66 +1,93 @@
-
-// importing user context
 const User = require("./model/user");
-
-// Register
-app.post("/register", (req, res) => {
-// our register logic goes here...
-});
-
-// Login
-app.post("/login", (req, res) => {
-// our login logic goes here
-});
-
-
+const auth = require("./middleware/auth");
 
 app.post("/register", async (req, res) => {
 
-    // Our register logic starts here
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }})
+  // Our register logic ends here
+
+
+  app.post("/login", async (req, res) => {
+
+    // Our login logic starts here
     try {
       // Get user input
-      const { first_name, last_name, email, password } = req.body;
+      const { email, password } = req.body;
   
       // Validate user input
-      if (!(email && password && first_name && last_name)) {
+      if (!(email && password)) {
         res.status(400).send("All input is required");
       }
-  
-      // check if user already exist
       // Validate if user exist in our database
-      const oldUser = await User.findOne({ email });
+      const user = await User.findOne({ email });
   
-      if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
+      if (user && (await bcrypt.compare(password, user.password))) {
+        // Create token
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+  
+        // save user token
+        user.token = token;
+  
+        // user
+        res.status(200).json(user);
       }
-  
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-  
-      // Create user in our database
-      const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-  
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      // save user token
-      user.token = token;
-  
-      // return new user
-      res.status(201).json(user);
+      res.status(400).send("Invalid Credentials");
     } catch (err) {
       console.log(err);
     }
-  
+    // Our register logic ends here
   });
-  
+
+  app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+  });
