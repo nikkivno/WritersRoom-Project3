b6d896a39d5223615e08e84935616fa5e53e70a1
nikkivno <nikkivigneault@gmail.com>
2023-10-03 15:42:35 -0300

    created and started code on about page

---
+++ src/components/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import '../styles/about.css';
+
+function About() {
+    return (
+        <div>
+            <div className='header'>
+                <h1 className='about'>About</h1>
+            </div>
+            <ul>
+                <li>What is writer's room?</li>
+                <li>Writers Room is a step by step tool for emerging writers based around the 3 Act Structure</li>
+                <br></br>
+                <li>What is the '3 Act Structure'?</li>
+                <li>The three act structure for storytelling is a way of plotting out a fictional novel so that the author can be sure to hit all of the main points</li>
+                <br></br>
+                <li>How does Writer's Room guide me through the planning process?</li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default About; 
