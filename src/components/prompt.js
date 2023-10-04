3dc055fc719e3767a86445e6bd2dc9c8d3dbcb7f
nikkivno <nikkivigneault@gmail.com>
2023-10-04 15:54:56 -0300

    added prompt files

---
+++ src/components/prompt.js
@@ -0,0 +1,14 @@
+// used for the first steps of new work. UI for the prompt generator
+
+import React, {useState} from 'react';
+import '../styles/prompt.css';
+
+function Prompt() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Prompt;
