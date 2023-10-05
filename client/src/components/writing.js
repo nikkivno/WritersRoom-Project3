abcea537bf7faf669f66ac7b6e9635253d30e4b6
nikkivno <nikkivigneault@gmail.com>
2023-10-05 20:10:14 -0300

    push for posterity

--- client/src/components/writing.js
+++ client/src/components/writing.js
@@ -25,7 +25,7 @@ export function Writing() {
           plugins: [
             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
             'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
           ],
           toolbar: 'undo redo | blocks | ' +
             'bold italic forecolor | alignleft aligncenter ' +
