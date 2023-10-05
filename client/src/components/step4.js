fee54fb47f46b32f31a86842b11b4dc40abf4ef1
nikkivno <nikkivigneault@gmail.com>
2023-10-05 19:51:32 -0300

    added in next button to writing section

--- client/src/components/step4.js
+++ client/src/components/step4.js
@@ -4,7 +4,12 @@ import '../styles/step4.css';
 function Step4() {
     return (
         <div>
-
+            <div>
+                <h1>Conclusion</h1>
+            </div>
+            <div>
+                <button><a href ='/writing' className={currentPage === '/writing' ? 'active' : ''}>Get Writing!</a></button>
+            </div>
         </div>
     )
 }
