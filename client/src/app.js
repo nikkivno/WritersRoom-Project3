7ec3307b54a8370994ef60bc3d6bf0026de14eb0
nikkivno <nikkivigneault@gmail.com>
2023-10-08 13:08:34 -0300

    client side routing for authentication added

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Navbar from './components/navbar';
 import Footer from './components/footer';
@@ -11,39 +12,51 @@ import Step4 from './components/step4';
 import Writing from './components/writing';
 
 function App() {
-    let Component
-    switch (window.location.pathname) {
+    const isAuthenticated = false;
+    // let Component
+    // switch (window.location.pathname) {
         // navbar
-        case "/":
-            Component = Login
-            break
-        case "/about":
-            Component = About
-            break  
-        case "/newwork":
-            Component = Prompt
-            break 
-            // in component transition to next steps in New Work process  
-        case "/step2":
-            Component = Step2
-            break
-        case '/step3':
-            Component = Step3
-            break
-        case '/step4':
-            Component = Step4
-            break
-        case '/writing':
-            Component = Writing
-            break
-    }
+        // case "/":
+        //     Component = Login
+        //     break
+        // case "/about":
+        //     Component = About
+        //     break  
+        // case "/newwork":
+        //     Component = Prompt
+        //     break 
+        //     // in component transition to next steps in New Work process  
+        // case "/step2":
+        //     Component = Step2
+        //     break
+        // case '/step3':
+        //     Component = Step3
+        //     break
+        // case '/step4':
+        //     Component = Step4
+        //     break
+        // case '/writing':
+        //     Component = Writing
+        //     break
     
     return (
-    <div>
+    
+        <BrowserRouter>
+        <div>
         <Navbar />
-        <Component />
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/about" component={About} />
+          <Route path="/newwork" component={isAuthenticated ? Prompt : Login} />
+          <Route path="/step2" component={isAuthenticated ? Step2 : Login} />
+          <Route path="/step3" component={isAuthenticated ? Step3 : Login} />
+          <Route path="/step4" component={isAuthenticated ? Step4 : Login} />
+          <Route path="/writing" component={isAuthenticated ? Writing : Login} />
+          <Redirect to="/" />
+        </Switch>
         <Footer />
-    </div>
+        </div>
+        </BrowserRouter>
     );
 }
 
