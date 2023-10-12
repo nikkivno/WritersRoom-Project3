d6fb93371451f35f57714e5b4766bceb0c58390d
nikkivno <nikkivigneault@gmail.com>
2023-10-12 10:55:53 -0300

    added in main page with title and sub

---
+++ client/src/components/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/main.css';
+
+function Main() {
+    return (
+        <div className='mainblock'>
+            <div className='header'>
+                <div className='text-container'>
+                    <h1 className='typewriter'>Writer's Room</h1>
+                </div>
+            </div>
+            <div>
+                <p>Let us help you write your first novel</p>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
