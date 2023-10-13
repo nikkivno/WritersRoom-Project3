f10366789e4f6264b3c76ca54f19deda48336138
Scoges <scott.ogrins@gmail.com>
2023-10-13 20:35:46 -0300

    display prompts in cards beside text editor

--- client/src/components/writing.js
+++ client/src/components/writing.js
@@ -1,16 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import "../styles/writing.css";
 import decode from "jwt-decode";
 
-export function Writing() {
+function Writing() {
   const editorRef = useRef(null);
+  const [userEmail, setUserEmail] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [catalystData, setCatalystData] = useState({});
+  const [midpointData, setMidpointData] = useState({});
+  const [endingData, setEndingData] = useState({});
+  const [promptId, setPromptId] = useState("");
+
+  // Original log function for saving content
   const log = async () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
 
-      const promptId = localStorage.getItem("promptId");
-      const novelId = localStorage.getItem("novelId");
       const token = localStorage.getItem("jwt");
       const userEmail = decode(token).email;
 
@@ -25,8 +31,8 @@ export function Writing() {
         };
 
         // if novel already exists, update it. Else, create it
-        if (novelId) {
-          reqUrl = `/api/novels/${novelId}`;
+        if (promptId) {
+          reqUrl = `/api/novels/${promptId}`;
           method = "PUT";
         } else {
           reqUrl = `/api/novels`;
@@ -49,6 +55,43 @@ export function Writing() {
       }
     }
   };
+
+  useEffect(() => {
+    // Fetch the user's email from localStorage
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      const email = decode(token).email;
+      setUserEmail(email);
+      setPromptId(localStorage.getItem("promptId"));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userEmail && promptId) {
+      // Fetch prompt data for the given promptId
+      fetchDataForUser(promptId)
+        .then((data) => {
+          setUserData(data);
+          setCatalystData(data.catalyst_input ? JSON.parse(data.catalyst_input) : {});
+          setMidpointData(data.midpoint_input ? JSON.parse(data.midpoint_input) : {});
+          setEndingData(data.ending_input ? JSON.parse(data.ending_input) : {});
+        })
+        .catch((error) => console.error("Error fetching user data: ", error));
+    }
+  }, [userEmail, promptId]);
+
+  const getCatalystData = () => {
+    return catalystData;
+  };
+
+  const getMidpointData = () => {
+    return midpointData;
+  };
+
+  const getEndingData = () => {
+    return endingData;
+  };
+
   return (
     <>
       <div>
@@ -56,15 +99,12 @@ export function Writing() {
       </div>
       <div className="full-container">
         <div className="texteditor">
-          <div className="userTitleContainer">
-            <input className="userTitle" placeholder="Story Title Here" />
-          </div>
+          {/* Your text editor component */}
           <Editor
             apiKey="1yd2u78it8w81i51bwc4b01pd50szutiv7ut912vsj5d0lq7"
             onInit={(evt, editor) => (editorRef.current = editor)}
             initialValue="<p>Start your story here!</p>"
-            init={{
-              height: 700,
+            init={{  height: 700,
               width: 1200,
               menubar: false,
               browser_spellcheck: true,
@@ -95,31 +135,52 @@ export function Writing() {
                 "alignright alignjustify | bullist numlist outdent indent | " +
                 "removeformat | help",
 
-              content_style:
+                content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
+            
           />
           <button onClick={log} className="submit">
-            save
+            Save
           </button>
         </div>
         <div className="card-container">
-          <div className="card" id="prompt">
-            <p>Prompt</p>
-          </div>
-          <div className="card" id="catylist">
-            <p>The Catylist</p>
-          </div>
-          <div className="card" id="midpoint">
-            <p>The Midpoint</p>
-          </div>
-          <div className="card" id="conclusion">
-            <p>The Conclusion</p>
-          </div>
-        </div>
+  <div className="card" id="prompt">
+    <p style={{fontSize: '12px' }}>Prompt</p>
+    <p style={{ fontSize: '8px' }}>{userData ? userData.prompt : "Prompt Not Found"}</p>
+  </div>
+  <div className="card" id="catalyst">
+    <p style={{ fontSize: '12px' }}>The Catalyst</p>
+    <p style={{ fontSize: '8px' }}>Characters: {getCatalystData().characters}</p>
+    <p style={{ fontSize: '8px' }}>Reason: {getCatalystData().reason}</p>
+  </div>
+  <div className="card" id="midpoint">
+    <p style={{ fontSize: '12px' }}>The Midpoint</p>
+    <p style={{ fontSize: '8px' }}>Coping Info: {getMidpointData().cope_info}</p>
+    <p style={{ fontSize: '8px' }}>Incident Consequence: {getMidpointData().incident_consequence}</p>
+  </div>
+  <div className="card" id="conclusion">
+    <p style={{ fontSize: '12px' }}>The Conclusion</p>
+    <p style={{ fontSize: '8px' }}>Before Climax: {getEndingData().before_climax}</p>
+    <p style={{ fontSize: '8px' }}>Climax: {getEndingData().climax}</p>
+  </div>
+</div>
       </div>
     </>
   );
+}
+
+async function fetchDataForUser(promptId) {
+  try {
+    const response = await fetch(`/api/prompts/${promptId}`);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Error fetching user data");
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 export default Writing;
