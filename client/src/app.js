8a03788d31b413dc5f635b344e6201f9e50e047e
Scoges <scott.ogrins@gmail.com>
2023-10-13 20:54:30 -0300

    display prompts in cards of text editor

--- client/src/app.js
+++ client/src/app.js
@@ -47,17 +47,17 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/newwork" element={isAuthenticated ? <Prompt /> : <Login />} />
-          <Route path="/step2" element={isAuthenticated ? <Step2 /> : <Login />} />
-          <Route path="/step3" element={isAuthenticated ? <Step3 /> : <Login />} />
-          <Route path="/step4" element={isAuthenticated ? <Step4 /> : <Login />} />
-          <Route path="/writing" element={isAuthenticated ? <Writing /> : <Login />} />
-          <Route path='/ongoingwork' element={isAuthenticated ? <Ongoingwork /> : <Login />} />
+          <Route path='/' element={<Main/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/newwork" element={isAuthenticated ? <Prompt/> : <Login/>} />
+          <Route path="/step2" element={isAuthenticated ? <Step2/> : <Login/>} />
+          <Route path="/step3" element={isAuthenticated ? <Step3/> : <Login/>} />
+          <Route path="/step4" element={isAuthenticated ? <Step4/> : <Login/>} />
+          <Route path="/writing" element={isAuthenticated ? <Writing/> : <Login/>} />
+          <Route path='/ongoingwork' element={isAuthenticated ? <Ongoingwork/> : <Login/>} />
         </Routes>
-        <Footer />
+        <Footer/>
       </div>
     </BrowserRouter>
   );
