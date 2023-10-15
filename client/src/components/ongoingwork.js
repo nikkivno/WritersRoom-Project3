97e2c2082e95c6bbb88f1801e932ce3a2190f703
nikkivno <nikkivigneault@gmail.com>
2023-10-15 20:39:26 -0300

    added a delete novel function to front end on ongoing work page, tested

--- client/src/components/ongoingwork.js
+++ client/src/components/ongoingwork.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; 
-import '../styles/ongoingwork.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styles/ongoingwork.css";
 
-import Trash from '../images/delete.png'
+import Trash from "../images/delete.png";
 
 function Ongoingwork() {
   const [novels, setNovels] = useState([]);
-  const currentUserEmail = 'seg@seg.com'; 
+  const currentUserEmail = "seg@seg.com";
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/novels?email=${currentUserEmail}`)
@@ -15,30 +15,55 @@ function Ongoingwork() {
         setNovels(data);
       })
       .catch((error) => {
-        console.error('Error fetching novels: ', error);
+        console.error("Error fetching novels: ", error);
       });
   }, [currentUserEmail]);
+
+  const handleDeleteNovel = (novelId) => {
+    fetch(`http://localhost:3000/api/novels/${novelId}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        fetch(`http://localhost:3000/api/novels?email=${currentUserEmail}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setNovels(data);
+          })
+          .catch((error) => {
+            console.error("Error fetching novels: ", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Error deleting novel: ", error);
+      });
+  };
 
   return (
     <div>
       <div className="work-header">
-        <h1 className="ongoingwork">Ongoing Work</h1>
+        <h1 className="ongoingwork">ongoing Work</h1>
       </div>
       <div className="book-covers">
-        {novels.map((novel) => (
-          <div className="book-cover" key={novel._id}>
-         
-            <Link
-              to={`/writing/${novel._id}?title=${novel.title}`}
-              style={{ textDecoration: 'none', color: 'inherit' }}
-            >
-              <h2>{novel.title}</h2>
-            </Link>
-            <div>
-              <img src={Trash} className='trash'/>
+        {novels
+          .filter((novel) => novel.title) // Filter out novels without a title
+          .map((novel) => (
+            <div key={novel._id} className="book-cover">
+              <Link
+                to={`/writing/${novel._id}?title=${novel.title}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+                key={novel._id}
+              >
+                <div className="book-cover">
+                  <h2>{novel.title}</h2>
+                </div>
+              </Link>
+                <img
+                  src={Trash}
+                  className="trash"
+                  onClick={() => handleDeleteNovel(novel._id)}
+                />
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
